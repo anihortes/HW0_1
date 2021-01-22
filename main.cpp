42eafe5918e6911cb976fe88aa2b16488c193e04
Adam Owen <ahowenjr@alaska.edu>
2021-01-21 22:30:09 -0900

    added more comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/*A. Harrison Owen
+ *
+ *
+ */
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -11,10 +16,13 @@ int main () {
     //fstream was chosen for its ability to read and write
     fstream newFile;
     string fileName;
-    cout << "Enter filename:";
+    //getting the file name should be more robust.
+    cout << "Enter filename (include '.txt'):";
     getline(cin, fileName);
-    //creates file/opens already existing file
-    newFile.open (fileName);
+    //Creates file/opens already existing file.
+    //Wipes data from file before user inputs their own.
+    newFile.open (fileName, fstream::out | fstream::trunc);
+    //If file is not found, exit program.
     if(!newFile){
         cout << "file not found.\n";
         return 0;
@@ -23,17 +31,17 @@ int main () {
     cout << "Enter text to print to the file:\n";
     string userText;
     getline(cin, userText);
+    //stops taking user input when user enters "exit".
+    //should be more robust
     while (userText != "exit") {
-        newFile << userText;
+        newFile << userText << endl;
+        cout << "USER TEXT HERE:" << userText << endl;
         cout << "Enter text to print to the file:\n";
-        string userText;
         getline(cin, userText);
         if (userText == "exit") break;
     }
     cout << "You are finished writing to the file.\n";
     cout << "Closing file.\n";
-//    newFile >> "dummy test";
-
     newFile.close();
     return 0;
 }
